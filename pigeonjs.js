b5892f398c0e6e9ab03f09fdbf3ea91dfc4b0648
KeriPMiller <miller.keripatricia@gmail.com>
2015-05-05 02:14:14 -0400

    added event scripts

--- pigeonjs.js
+++ pigeonjs.js
@@ -1,5 +1,43 @@
-$(document).ready({ function(){
+$(document).ready(function(){
+	$("#gallery-nav li a").mouseenter(function(){
+		$(this).animate({
+			color: "green"});
+		console.log("mouse over nav link");
+	});
+	$("#gallery-nav li a").mouseleave(function(){
+		$(this).animate({
+			color:"#7fd47f"
+		});
+		console.log("mouseleave nav link");
+	});
+	$("#services-nav li a").mouseenter(function(){
+		$(this).animate({
+			color: "#4d1b7b"});
+		console.log("mouse over nav link");
+	});
+	$("#services-nav li a").mouseleave(function(){
+		$(this).animate({
+			color:"#ac6fc7"
+		});
+		console.log("mouseleave nav link");
+	});
 	$(".gallery").mouseenter(function(){
-		console.log('mouse is in');
+		 $(this).children(".hover-content").show("fade", 500);
+		console.log("mouse is in");
+	});
+	$(".gallery").mouseleave(function(){
+			$(this).children(".hover-content").hide("fade", 500);
+		console.log("mouse is out");
+	});
+	// expand
+	$(".gallery").click(function(){
+		$(this).addClass("big");
+		$(this).children(".hover-content").css({"backgroundColor": "orange", "opacity":".5"});
+		console.log("regular click only");
+	});
+		$(".big").click(function(){
+		$(this).removeClass("big");
+		$(this).children(".hover-content").css({"backgroundColor": "#49433c", "opacity":".9"});
+		console.log("big class clicked");
 	});
 });
